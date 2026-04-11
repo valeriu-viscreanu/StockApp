7ce763945058dd3da3baa70d6c1713ad3ef544f0
Valeriu Daniel Viscreanu <valeriu.viscreanu@gmail.com>
2026-04-11 11:15:30 +0300

    add migration to assign default role to existing users

---
+++ .Net/src/StockApp.Infrastructure/Migrations/20260410070707_AssignDefaultRoleToExistingUsers.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StockApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AssignDefaultRoleToExistingUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
