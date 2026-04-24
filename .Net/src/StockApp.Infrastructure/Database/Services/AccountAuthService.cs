79ea6ba4a42e3d4f40e5d07283aa118bd94a9eb0
Valeriu Daniel Viscreanu <valeriu.viscreanu@gmail.com>
2026-04-24 15:48:32 +0300

    Implement Register method in AccountAuthService to persist users to database

--- .Net/src/StockApp.Infrastructure/Database/Services/AccountAuthService.cs
+++ .Net/src/StockApp.Infrastructure/Database/Services/AccountAuthService.cs
@@ -1,5 +1,6 @@
 using StockApp.Application.DTO;
 using StockApp.Application.ServiceContracts;
+using StockApp.Domain.Entities;
 using StockApp.Infrastructure.Database;
 using System;
 using System.Linq;
@@ -18,7 +19,8 @@ namespace StockApp.Infrastructure.Database.Services
         public LoginResponse Login(LoginRequest loginRequest)
         {
             var user = _db.Users.FirstOrDefault(u => u.Email == loginRequest.Email);
-            if (user != null && loginRequest.Password == "123") // Hardcoded for tutorial
+            // Note: Password hashing is omitted as per current tutorial state ("123" hardcode)
+            if (user != null && loginRequest.Password == "123") 
             {
                 return new LoginResponse
                 {
@@ -32,6 +34,47 @@ namespace StockApp.Infrastructure.Database.Services
 
         public RegisterResponse Register(RegisterRequest registerRequest)
         {
+            if (_db.Users.Any(u => u.Email == registerRequest.Email))
+            {
+                return new RegisterResponse { IsSuccess = false, ErrorMessage = "Email already exists" };
+            }
+
+            var userId = Guid.NewGuid();
+            var newUser = new ApplicationUser
+            {
+                UserID = userId,
+                Email = registerRequest.Email,
+                RoleID = registerRequest.RoleID ?? Guid.Parse("10000000-0000-0000-0000-000000000001") // Default to Customer
+            };
+
+            var newAccount = new Account
+            {
+                AccountID = Guid.NewGuid(),
+                UserID = userId,
+                Balance = 1000.00, // Initial balance
+                DateOfBirth = DateTime.UtcNow.AddYears(-20) // Default DOB as it's not in DTO
+            };
+
+            var userDetails = new UserDetails
+            {
+                DetailsID = Guid.NewGuid(),
+                UserID = userId,
+                Street = registerRequest.Street,
+                StreetNumber = registerRequest.StreetNumber,
+                Building = registerRequest.Building,
+                Unit = registerRequest.Unit,
+                City = registerRequest.City,
+                ZipCode = registerRequest.ZipCode,
+                Country = registerRequest.Country,
+                AdditionalInfo = registerRequest.AdditionalInfo,
+                PhoneNumber = registerRequest.PhoneNumber
+            };
+
+            _db.Users.Add(newUser);
+            _db.Accounts.Add(newAccount);
+            _db.UserDetails.Add(userDetails);
+            _db.SaveChanges();
+
             return new RegisterResponse { IsSuccess = true };
         }
     }
